748d50de05717d80dd265aad8092af9a33e40318
Brian T. Rice <briantrice@gmail.com>
2024-03-29 16:32:22 -0700

    Fix display of negative Log-Log decade markers to use powers of ten.

--- SlideRule.py
+++ SlideRule.py
@@ -426,7 +426,11 @@ def draw_numeral(r, color, y_off, num, x, y, font_size, font_style, al):
     elif not num.is_integer():
         num_sym = str(num)
         if num_sym.startswith('0.'):
-            num_sym = num_sym[1:]  # Omit leading zero digit
+            expon = math.log10(num)
+            if expon.is_integer() and abs(expon) > 2:
+                num_sym = f'10^{int(expon)}'
+            else:
+                num_sym = num_sym[1:]  # Omit leading zero digit
     elif num == 0:
         num_sym = '0'
     else:
