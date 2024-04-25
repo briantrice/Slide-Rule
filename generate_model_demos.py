a9690def7b54d094e2e26594f9e9b1db668b3850
Brian T. Rice <briantrice@gmail.com>
2024-04-24 17:05:43 -0700

    generate_model_demos script and a resulting examples directory with all the current models in all modes.
    Stickerprint support is uneven, requiring more geometry parametrization.

---
+++ generate_model_demos.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import argparse
+import os.path
+
+from SlideRule import (
+    Models, keys_of,
+    render_diagnostic_mode, render_sliderule_mode, render_stickerprint_mode
+)
+
+
+def main():
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--model',
+                             choices=keys_of(Models),
+                             default=None,
+                             help='Which sliderule model (all by default)')
+    cli_args = args_parser.parse_args()
+    base_dir = os.path.relpath('examples/')
+    os.makedirs(base_dir, exist_ok=True)
+    for model_name in ([cli_args.model] if cli_args.model else keys_of(Models)):
+        print(f'Building example outputs for: {model_name}')
+        model = getattr(Models, model_name)
+
+        print(f'Building Diagnostic output for: {model_name}')
+        diagnostic_img = render_diagnostic_mode(model)
+        diagnostic_img.save(os.path.join(base_dir, f'{model_name}.Diagnostic.png'))
+
+        print(f'Building SlideRuleScales output for: {model_name}')
+        sliderule_img = render_sliderule_mode(model, borders=True, cutoffs=True)
+        sliderule_img.save(os.path.join(base_dir, f'{model_name}.SlideRuleScales.png'), 'PNG')
+
+        print(f'Building StickerCut output for: {model_name}')
+        sliderule_stickers_img = render_sliderule_mode(model)
+        stickers_img = render_stickerprint_mode(model, sliderule_stickers_img)
+        stickers_img.save(os.path.join(base_dir, f'{model_name}.StickerCut.png'))
+
+
+if __name__ == '__main__':
+    main()
